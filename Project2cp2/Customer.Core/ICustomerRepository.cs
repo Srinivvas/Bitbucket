4889035f249ad9e2d52e057c66c5db2cfbd14204
Srinivvas <Srinivvas@users.noreply.github.com>
2019-06-15 18:21:46 +0800

    Interfacs

---
+++ Project2cp2/Customer.Core/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Core
+{
+    public interface ICustomerRepository
+    {
+
+        // IEnumerable<Customer> CustList(Customer Id);
+        Customer FindByCustID(int Id);
+        //  List<Customer> searchdetails(int Id);
+        Customer FindByEmail(string Email);
+        void AddCustomer(Customer c);
+    }
+}
